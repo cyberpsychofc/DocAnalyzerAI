543957ab59f86bb009f58cbc5423898444c5cde5
Om Aryan <aryan2003k@gmail.com>
2024-07-15 22:34:59 +0530

    minor improv | file upl

---
+++ DocAnalyzer/src/main/java/org/cyberpsych/docanalyzer/services/storage/StorageProperties.java
@@ -0,0 +1,15 @@
+package org.cyberpsych.docanalyzer.services.storage;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+    private String location = "upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
