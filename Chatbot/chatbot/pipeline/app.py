51b255926f70194012c8cbc69de1ee658c390b16
Om Aryan <aryan2003k@gmail.com>
2024-07-18 09:20:18 +0530

    file loader done

--- Chatbot/chatbot/pipeline/app.py
+++ Chatbot/chatbot/pipeline/app.py
@@ -1,6 +1,6 @@
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain.chains.retrieval import create_retrieval_chain
@@ -12,7 +12,7 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 
-load_dotenv
+load_dotenv()
 
 os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
 os.environ["LANGCHAIN_TRACING"]="true"
@@ -28,4 +28,23 @@ You will be reward you positvely if the user finds the answer helpful.
 </context>
 Question: {input}""")
 
-#loaders to be implemented
+#loading the file and its extension
+src = ""
+for (root,dirs,files) in os.walk('./../../../upload-dir', topdown=False):
+    src = files[0]
+file_path = './../../../upload-dir/data.'
+extension = src[(len(src) - 3):]
+
+#data-ingestion
+loader = None
+
+if extension == "txt":
+    loader = TextLoader(file_path + extension)
+if extension == "pdf":
+    loader = PyPDFLoader(file_path + extension)
+
+doc = loader.load()
+doc_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+document = doc_splitter.split_documents(doc)
+
+#embedding to be done
